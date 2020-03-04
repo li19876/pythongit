e38a2af5be9a762523ce3dda8bfea30b9481f4ea
li19876 <1462063555qq.com>
2020-03-04 11:20:18 +0800

    as

--- 图片p文字.py
+++ 图片p文字.py
@@ -5,27 +5,27 @@ import os
 
 
 # 加载底图
-def hec(bgimg, fimg, box):
+def hec(bgimg, text):
     base_img = Image.open(bgimg)
-    resdir = os.path.dirname(bgimg) + os.sep + os.path.basename(bgimg)[:-4]
-    isExists = os.path.exists(resdir)
-    if not isExists:
-        os.mkdir(resdir)
-        os.chdir(resdir)
-    else:
-        os.chdir(resdir)
+    # resdir = os.path.dirname(bgimg) + os.sep + os.path.basename(bgimg)[:-4]
+    # isExists = os.path.exists(resdir)
+    # if not isExists:
+    #     os.mkdir(resdir)
+    #     os.chdir(resdir)
+    # else:
+    #     os.chdir(resdir)
     # 可以查看图片的size和mode，常见mode有RGB和RGBA，RGBA比RGB多了Alpha透明度
     # print base_img.size, base_img.mode
     # box = (334, 130, 640, 673)  # 底图上需要P掉的区域
 
     # 加载需要P上去的图片
-    tmp_img = Image.open(fimg)
-    fimgname = os.path.basename(fimg)
-    print(fimgname)
+    # tmp_img = Image.open(fimg)
+    imgname = os.path.basename(bgimg)
+    print(imgname)
     # 这里可以选择一块区域或者整张图片
     # region = tmp_img.crop((0,0,304,546)) #选择一块区域
     # 或者使用整张图片
-    region = tmp_img
+    # region = tmp_img
     # text = ''
     # for i in fimgname[:-4]:
     #     text += i + "  "
@@ -34,19 +34,21 @@ def hec(bgimg, fimg, box):
     # 注意，region的大小必须和box的大小完全匹配。但是两张图片的mode可以不同，合并的时候回自动转化。如果需要保留透明度，则使用RGMA mode
     # 提前将图片进行缩放，以适应box区域大小
     # region = region.rotate(180) #对图片进行旋转
-    region = region.resize((box[2] - box[0], box[3] - box[1]))
-    base_img.paste(region, box)
+    # region = region.resize((box[2] - box[0], box[3] - box[1]))
+    # base_img.paste(region, box)
     draw = ImageDraw.Draw(base_img)  # 修改图片
-    # font = ImageFont.truetype('C:/Windows/Fonts/SourceHanSansK-Bold.ttf', 15)
-    # draw.text((114, 327), text, fill=(0, 0, 0), font=font)  # 利用ImageDraw的内置函数，在图片上写入文字
+    x=3050+((1400-((len(text)-3)*90))/2)
+    font = ImageFont.truetype('C:/Windows/Fonts/SourceHanSansK-Bold.ttf', 90)
+    draw.text((x, 573), text, fill=(0, 0, 0), font=font)  # 利用ImageDraw的内置函数，在图片上写入文字
     # base_img.show() # 查看合成的图片
-    base_img.save(fimgname)  # 保存图片
+    base_img.save('C:\\Users\\Administrator\\Desktop\\tongliao\\'+text[:6]+'.jpg')  # 保存图片
 
 
 if __name__ == '__main__':
     fimg = e.diropenbox("选择二维码的文件夹")
-    bgimg=e.fileopenbox("选择底图")
-    print(os.path.dirname(bgimg))
+    # bgimg=e.fileopenbox("选择底图")
+    # print(os.path.dirname(bgimg))
+
     filenames= os.walk(fimg)
     # print(fimg)
     files =[]
@@ -55,7 +57,12 @@ if __name__ == '__main__':
         files =file
     for f in files[2]:
         filelist.append(files[0]+os.sep+f)
-    box = (3050,700,4450,2100)
+    with open("tongliao.txt",'r',encoding='utf-8') as f:
+        for i in f:
+            list1=[s for s in i.split(',')]
+
+    # box = (3050,700,4450,2100)
     for i in filelist:
-        hec(bgimg,i,box)
+        text= list1.pop(0)
+        hec(i,text)
 
---
+++ 批量改尺寸.py
@@ -0,0 +1,39 @@
+import easygui as e
+import os
+from PIL import Image
+def hec(img):
+    base_img = Image.open(img)
+    imgname = os.path.basename(img)
+    # base_img=base_img.resize((3509,2481))  # A4
+    # base_img.save(imgname[:-4]+'_A4.jpg')
+    # print(imgname[:-4]+'_A4.jpg')
+    base_img = base_img.resize((4962, 3509))  # A3
+    base_img.save(imgname[:-4] + '_A3.jpg')
+    # print(imgname[:-4] + '_A3.jpg')
+if __name__ == '__main__':
+    fimg = e.diropenbox("选择图片文件夹")
+    # bgimg=e.fileopenbox("选择底图")
+    # print(os.path.dirname(bgimg))
+
+    filenames= os.walk(fimg)
+    # print(filenames)
+    files =[]
+    filelist=[]
+    for file in filenames:
+        # print(file)
+        files =file
+        break
+    # print(files)
+    for f in files[2]:
+        filelist.append(files[0]+os.sep+f)
+    resdir = fimg + os.sep + 'A3'
+    isExists = os.path.exists(resdir)
+    if not isExists:
+        os.mkdir(resdir)
+        os.chdir(resdir)
+    else:
+        os.chdir(resdir)
+    # print(filelist)
+    for i in filelist:
+        hec(i)
+        print(i)
