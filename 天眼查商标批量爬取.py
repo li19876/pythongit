cde2298901ccbdcfad0c995241dbc2ea652314e6
li19876 <1462063555qq.com>
2020-03-26 11:43:09 +0800

    添加断点重连机制

--- 天眼查商标批量爬取.py
+++ 天眼查商标批量爬取.py
@@ -24,7 +24,7 @@ def getres(url):
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
     }
-    cookie = 'TYCID=f2564d2097f311e9b4169345e5b2d504; undefined=f2564d2097f311e9b4169345e5b2d504; ssuid=4261586970; _ga=GA1.2.238743067.1561540964; __insp_wid=677961980; __insp_slim=1565856225188; __insp_nv=true; __insp_targlpu=aHR0cHM6Ly93d3cudGlhbnlhbmNoYS5jb20vY2xhaW0vYXBwbHkvMzM2MTM4MTQyMT9wcmljaW5nUGFja2FnZT0wJmVkaXRNb2JpbGU9MQ%3D%3D; __insp_targlpt=5aSp55y85p_lLeWVhuS4muWuieWFqOW3peWFt1%2FkvIHkuJrkv6Hmga%2Fmn6Xor6Jf5YWs5Y_45p_l6K_iX_W3peWVhuafpeivol%2FkvIHkuJrkv6HnlKjkv6Hmga%2Fns7vnu58%3D; jsid=SEM-BAIDU-PP-VI-301001; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1574995359,1575967374,1577427146; _gid=GA1.2.272920010.1577427146; RTYCID=bb1b80f2d3fc41a3af52f375f9236a33; CT_TYCID=9b039d9169624b44a8cacf71d5703964; cloud_token=a53846ad75b8432086543b92ec8b9066; bannerFlag=true; aliyungf_tc=AQAAAHyEQVpbBwYArXwvaljnelrHTL9U; csrfToken=s60CO2Rk8zkccTgIKujXaeCE; href=https%3A%2F%2Fip.tianyancha.com%2Ftm%2F35857713t45; Hm_lvt_01daffb1fc8d135428513cb7c4afca15=1577427375; CLOUDID=fea302fe-2ba0-44a4-9897-2c99778df91b; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1577427520; accessId=b3ebcec0-09a9-11ea-b7e1-aff0bff10886; qimo_seosource_b3ebcec0-09a9-11ea-b7e1-aff0bff10886=%E7%AB%99%E5%86%85; qimo_seokeywords_b3ebcec0-09a9-11ea-b7e1-aff0bff10886=; pageViewNum=32; Hm_lpvt_01daffb1fc8d135428513cb7c4afca15=1577433349'
+    cookie = 'TYCID=f2564d2097f311e9b4169345e5b2d504; undefined=f2564d2097f311e9b4169345e5b2d504; ssuid=4261586970; _ga=GA1.2.238743067.1561540964; __insp_wid=677961980; __insp_slim=1565856225188; __insp_nv=true; __insp_targlpu=aHR0cHM6Ly93d3cudGlhbnlhbmNoYS5jb20vY2xhaW0vYXBwbHkvMzM2MTM4MTQyMT9wcmljaW5nUGFja2FnZT0wJmVkaXRNb2JpbGU9MQ%3D%3D; __insp_targlpt=5aSp55y85p_lLeWVhuS4muWuieWFqOW3peWFt1%2FkvIHkuJrkv6Hmga%2Fmn6Xor6Jf5YWs5Y_45p_l6K_iX_W3peWVhuafpeivol%2FkvIHkuJrkv6HnlKjkv6Hmga%2Fns7vnu58%3D; tyc-user-phone=%255B%252218202610240%2522%255D; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1583198527,1583298144,1583366654,1583922481; bannerFlag=true; _gid=GA1.2.1548451223.1585017829; cloud_token=5fe4ec56c0304f9c9aeac29094456a39; aliyungf_tc=AQAAAEB2BDQKzgUASEwvatSrWxfGHEGP; csrfToken=XtNxROO6rMT3ETZdc7TPz4f_; Hm_lvt_2530ab7750ce2f0d714e3e69613d481b=1585128690; token=6bde4d4261354d479f44a37d00468634; _utm=6e3d249395644c098789108f4ab6e8f2; tyc-user-info=%257B%2522claimEditPoint%2522%253A%25220%2522%252C%2522vipToMonth%2522%253A%2522false%2522%252C%2522explainPoint%2522%253A%25220%2522%252C%2522integrity%2522%253A%252210%2525%2522%252C%2522state%2522%253A%25223%2522%252C%2522surday%2522%253A%2522125%2522%252C%2522announcementPoint%2522%253A%25220%2522%252C%2522schoolGid%2522%253A%2522%2522%252C%2522bidSubscribe%2522%253A%2522-1%2522%252C%2522vipManager%2522%253A%25220%2522%252C%2522onum%2522%253A%252210%2522%252C%2522monitorUnreadCount%2522%253A%25220%2522%252C%2522discussCommendCount%2522%253A%25220%2522%252C%2522claimPoint%2522%253A%25220%2522%252C%2522token%2522%253A%2522eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODIwMjYxMDI0MCIsImlhdCI6MTU4NTEzMTkyNiwiZXhwIjoxNjAwNjgzOTI2fQ.Zul7ephCWp4RKZ9Ua1O1CtzGJoFtl302JAp1tmVCC3TkhTnjA0mEdCXWcada_FovYB-F6oi__0lXNWRSiQObIg%2522%252C%2522schoolAuthStatus%2522%253A%25222%2522%252C%2522vipToTime%2522%253A%25221595903278098%2522%252C%2522redPoint%2522%253A%25220%2522%252C%2522companyAuthStatus%2522%253A%25222%2522%252C%2522myAnswerCount%2522%253A%25220%2522%252C%2522myQuestionCount%2522%253A%25220%2522%252C%2522signUp%2522%253A%25220%2522%252C%2522nickname%2522%253A%2522%25E9%2599%2588%25E7%25BE%258E%2522%252C%2522privateMessagePointWeb%2522%253A%25220%2522%252C%2522privateMessagePoint%2522%253A%25220%2522%252C%2522isClaim%2522%253A%25220%2522%252C%2522isExpired%2522%253A%25220%2522%252C%2522pleaseAnswerCount%2522%253A%25220%2522%252C%2522bizCardUnread%2522%253A%25220%2522%252C%2522vnum%2522%253A%25220%2522%252C%2522companyGid%2522%253A%2522%2522%252C%2522mobile%2522%253A%252218202610240%2522%257D; auth_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODIwMjYxMDI0MCIsImlhdCI6MTU4NTEzMTkyNiwiZXhwIjoxNjAwNjgzOTI2fQ.Zul7ephCWp4RKZ9Ua1O1CtzGJoFtl302JAp1tmVCC3TkhTnjA0mEdCXWcada_FovYB-F6oi__0lXNWRSiQObIg; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1585131962; _gat_gtag_UA_123487620_1=1; Hm_lpvt_2530ab7750ce2f0d714e3e69613d481b=1585193950'
     cookies = {i.split("=")[0]: i.split("=")[1] for i in cookie.split("; ")}
     res = requests.get(url, headers=headers, cookies=cookies)
     return res
@@ -62,7 +62,7 @@ def parse_detail(response):
     item["u"] = html.xpath("string(//td[text()='注册公告日期']/following-sibling::td[1])")
     item["v"] = html.xpath("string(//td[text()='商标公告']/following-sibling::td[1])")
     item["w"] = html.xpath("string(//td[text()='商品／服务']/following-sibling::td[1])")
-    item["x"] = html.xpath("string(//td[text()='商标流程']/following-sibling::td[1])")
+    item["x"] = html.xpath("string(//td[text()='商标流程']/following-sibling::td[1])").replace('\xa0',' ')
     return item
 
 
@@ -107,21 +107,28 @@ def getpage(response):
         return None
 
 
-def run():
+"""
+:param big 总分类url索引开始位置,从0开始
+:param p   从多少页开始,从0开始,传0就是第一页
+:param sm  从当前页的多少条开始,值为0-19
+"""
+
+def run(big,p,sm):
     urllist = []
     with open("商标url.txt", 'r') as f:
         for i in f:
             urllist.append(i.strip())  # 获取总类别文件url,也就是各分类的url
-    for url in urllist:
+    for url in urllist[big:]:
         print(url)
         res = getres(url)
         page = getpage(res)
         if page is not None:
             pageurl = [url + "/p" + str(i) for i in range(1, page + 1)]  # 获取分页的url
-            for i in pageurl:
+            for i in pageurl[p:]:
                 r = getres(i)
+                print('当前分页链接是'+i)
                 deteil_urllist = parse_listpage(r)  # 获取详情页url
-                for s in deteil_urllist:
+                for s in deteil_urllist[sm:]:
                     res = getres(s)
                     item = parse_detail(res)
                     save(item)
@@ -144,5 +151,5 @@ if __name__ == '__main__':
     #                 f.write(s+'\n')
     #                 print('写入了'+s)
     #         time.sleep(2)
-    run()
+    run(0,5,4)
 #
